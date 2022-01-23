8dd827052fc3898a8575a98b979484589fa69591
NuzairaFarin <91980185+NuzairaFarin@users.noreply.github.com>
2022-01-23 15:27:22 -0500

    01232022 3:27PM

---
+++ src/hw5Q3Constructor/Student.java
@@ -0,0 +1,20 @@
+package hw5Q3Constructor;
+
+public class Student {
+	public String stName;
+	public int stID;
+	public char sex;
+	public boolean isProgrammer;
+	public float grade;
+
+	public Student() {
+
+	}
+
+	public Student(String stName, int stID, char sex, boolean isProgrammer, float grade) {
+
+		System.out.println("This is from default Constructor of Student class." + "\nStudent Name:" + stName + "ID:"
+				+ stID + ", Sex:" + sex + ", Grade=" + grade + " and Java Programmer? Ans:" + isProgrammer);
+	}
+
+}
---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,13 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		Student student01 = new Student("Nuzaira F, ", 23706065, 'F', true, 3.976f);
+		Student student02 = new Student("Nabila N, ", 23706045, 'F', true, 2.98f);
+		Student student03 = new Student("Dhamer H, ", 23706078, 'F', false, 2.88f);
+
+	}
+
+}
