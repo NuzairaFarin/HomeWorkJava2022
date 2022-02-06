2b111b5acfc87916c7e38f4bdb98e6093859f33a
NuzairaFarin <91980185+NuzairaFarin@users.noreply.github.com>
2022-02-06 10:20:20 -0500

    02062022

--- src/hw2Q2JavaBasics/package-info.java
+++
@@ -1,1 +0,0 @@
-package hw2Q2JavaBasics;
--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -1,16 +1,5 @@
 package hw6Q2IfElseCondition;
 
-/*2) Create a package name --> "hw6Q2IfElseCondition" inside your HW project. 
- * Create a Class, name "Voter". Type the main method. 
- * After the main method, define a int type variable age and initialize your age. 
- * Now write some condition. 
- * a) your age is equal to 18, outcome will be "I am a Voter" 
- * b) your age is less than 18, outcome will be "I am not a Voter 
- * c) your age is greater than 18 or equal to 18 , 
- * outcome will be "I am a Voter (>=) d) 
- * And finally the outcome will be "Please add a valid age". 
- * In the above 4 condition, 
- * use appropriate key word like if, else if, else to execute that you are a voter or not*/
 public class Voter {
 
 	public static void main(String[] args) {
