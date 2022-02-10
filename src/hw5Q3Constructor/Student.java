f9ce429b5888d55efbc9dd6c8525930a7dfba9cb
NuzairaFarin <91980185+NuzairaFarin@users.noreply.github.com>
2022-02-10 12:52:05 -0500

    02102022 12:51 PM

--- src/hw5Q3Constructor/Student.java
+++ src/hw5Q3Constructor/Student.java
@@ -12,9 +12,15 @@ public class Student {
 	}
 
 	public Student(String stName, int stID, char sex, boolean isProgrammer, float grade) {
-
+		super();
+		this.stName = stName;
+		this.stID = stID;
+		this.sex = sex;
+		this.isProgrammer = isProgrammer;
+		this.grade = grade;
 		System.out.println("This is from default Constructor of Student class." + "\nStudent Name:" + stName + "ID:"
 				+ stID + ", Sex:" + sex + ", Grade=" + grade + " and Java Programmer? Ans:" + isProgrammer);
+
 	}
 
 }
--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -4,18 +4,15 @@ public class Voter {
 
 	public static void main(String[] args) {
 
-		int myAge = 23;
-		int age1 = 18;
-
-		if (myAge == age1) {
-			System.out.println("I am a Voter.");
-		} else if (myAge < age1) {
-			System.out.println("I am not a Voter.");
-		} else if (myAge >= age1) {
-			System.out.println("I am a Voter.");
+		int age = 23;
+		if (age == 18) {
+			System.out.println("I am a Voter");
+		} else if (age < 18) {
+			System.out.println("I am not a Voter");
+		} else if (age > 18) {
+			System.out.println("I am a Voter");
 		} else {
-			System.out.println("Please add a valid age.");
+			System.out.println("Please add a valid age");
 		}
 	}
-
 }
