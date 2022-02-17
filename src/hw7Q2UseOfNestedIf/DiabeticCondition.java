5428e5798273d40f1d3936c65f9f4a42b9a5cea9
NuzairaFarin <nzrfarin@gmail.com>
2022-02-16 22:15:33 -0500

    02162022

--- src/hw7Q2UseOfNestedIf/DiabeticCondition.java
+++ src/hw7Q2UseOfNestedIf/DiabeticCondition.java
@@ -14,7 +14,7 @@ public class DiabeticCondition {
 			if (hbga1c >= 5.7) {
 				System.out.println("I am a pre-diabetic patient");
 			} else if (hbga1c < 5.7) {
-				System.out.println("I am a healthy personmncbdj");
+				System.out.println("I am a healthy person");
 			}
 		}
 		scanner.close();
